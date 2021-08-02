3e3d7b9695a6e856933e2a9bde936f2377080c8a
ahb.linux <ahb.linux>
2021-08-02 12:45:08 +0200

    adding new chatapp

---
+++ chatapp/src/App.js
@@ -0,0 +1,18 @@
+import {ChatEngine} from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height= "100vh"
+            projectID = "a7f09fba-6da8-4d55-8162-a74c9de8f18c"
+            userName = "admin"
+            userSecret = "alvaro4954"
+            />
+    ); 
+
+}
+        
+        
+export default App;
