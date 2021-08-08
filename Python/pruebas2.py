5faac28d749f7bf6facbe8db43cb2c884e680b4a
alvaroherbon <alvaroherbon93@gmail.com>
2021-08-08 12:55:28 +0200

    adding new files

---
+++ Python/pruebas2.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        num1 = 0
+        num2 = 0
+        val = l1.val
+        while val != None:
+            num1 += val
+            val = l1.next
